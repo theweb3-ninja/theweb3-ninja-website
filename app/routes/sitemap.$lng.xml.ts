016c9a176be603157928cc29cbcf93707999cd0c
xdemocle <xdemocle@gmail.com>
2025-08-23 20:05:34 +0100

    fix: improve sitemap generation logic and route handling for multilingual support

--- app/routes/sitemap.$lng.xml.ts
+++ app/routes/sitemap.$lng.xml.ts
@@ -9,37 +9,49 @@ const replacePath = (lng?: string, path?: string, defaultLng?: string): string =
 };
 
 const rewriteRoutes = (routes: ServerBuild['routes'], lng?: string, defaultLng?: string) => {
-  if (!routes) return [];
+  if (!routes) {
+    return [];
+  }
 
   return Object.values(routes)
     .map(route => {
       const path = replacePath(lng, route?.path, defaultLng);
 
-      return {
-        id: route?.id,
-        path: path === '' ? '/' : path,
-        lng,
-      };
+      if (route && route.path) {
+        route.path = path === '' ? '/' : path;
+      }
+
+      return route;
     })
-    .filter(route => route.path && !route.path.match(/[*:]/));
+    .filter(route => route?.path && !route?.path.match(/[*:]/));
 };
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const serverBuild = await import('virtual:react-router/server-build');
   const { routes } = serverBuild;
-  const { origin } = new URL(request.url);
+  const { host } = new URL(request.url);
   const lng = params.lng;
-  let routeBaseEntries: { id: string | undefined; path: string; lng: string | undefined }[] = [];
-
-  if (lng === getDefaultLanguage()) {
-    routeBaseEntries = rewriteRoutes(routes, lng, getDefaultLanguage());
-  }
 
   const routeEntries = rewriteRoutes(routes, lng);
 
+  if (lng === getDefaultLanguage()) {
+    const routeBaseEntries = rewriteRoutes(routes, lng, getDefaultLanguage());
+
+    const sitemap = await generateRemixSitemap({
+      domain: host,
+      routes: [...routeBaseEntries, ...routeEntries] as unknown as ServerBuild['routes'],
+    });
+
+    return new Response(sitemap, {
+      headers: {
+        'Content-Type': 'application/xml',
+      },
+    });
+  }
+
   const sitemap = await generateRemixSitemap({
-    domain: origin,
-    routes: [...routeBaseEntries, ...routeEntries],
+    domain: host,
+    routes: routeEntries as unknown as ServerBuild['routes'],
   });
 
   return new Response(sitemap, {
